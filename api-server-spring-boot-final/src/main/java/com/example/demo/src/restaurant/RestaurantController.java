fc4c27ac24701c2a706edc3a2ebbcbb5ea72f582
Halfmbbn <halfmbbn@gmail.com>
2022-05-23 05:33:08 +0900

    식당 조회 api 추가

--- api-server-spring-boot-final/src/main/java/com/example/demo/src/restaurant/RestaurantController.java
+++ api-server-spring-boot-final/src/main/java/com/example/demo/src/restaurant/RestaurantController.java
@@ -1,10 +1,16 @@
 package com.example.demo.src.restaurant;
 
+import com.example.demo.config.BaseException;
+import com.example.demo.config.BaseResponse;
+import com.example.demo.src.restaurant.model.GetRestaurantRes;
+import com.example.demo.src.user.model.GetUserRes;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/restaurants")
@@ -18,6 +24,23 @@ public class RestaurantController {
     public RestaurantController(RestaurantProvider provider, RestaurantService service) {
         this.provider = provider;
         this.service = service;
+    }
+    @ResponseBody
+    @GetMapping("")
+    public BaseResponse<List<GetRestaurantRes>> getRestaurant(@RequestParam(value = "search-mode",required = false) String search_mode,
+                                                              @RequestParam(value = "lat",required = false) Double latitude,
+                                                              @RequestParam(value = "long",required = false) Double longitude,
+                                                              @RequestParam(value = "user-id",required = false) Long userId,
+                                                              @RequestParam(value = "food-category",required = false) List<Integer> foodCategories){
+        logger.info("user lat -> ", latitude);
+        logger.info("user long -> ", longitude);
+
+        try{
+            List<GetRestaurantRes> getRestaurantRes = provider.getRestaurant(latitude, longitude, foodCategories.toString().replace("[","(").replace("]",")"));
+            return new BaseResponse<>(getRestaurantRes);
+        } catch(BaseException exception){
+            return new BaseResponse<>((exception.getStatus()));
+        }
     }
 
 
--- api-server-spring-boot-final/src/main/java/com/example/demo/src/restaurant/RestaurantProvider.java
+++ api-server-spring-boot-final/src/main/java/com/example/demo/src/restaurant/RestaurantProvider.java
@@ -1,8 +1,16 @@
 package com.example.demo.src.restaurant;
 
+import com.example.demo.config.BaseException;
+import com.example.demo.config.BaseResponse;
+import com.example.demo.src.restaurant.model.GetRestaurantRes;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+import static com.example.demo.config.BaseResponseStatus.DATABASE_ERROR;
 
 @Service
 public class RestaurantProvider {
@@ -12,5 +20,15 @@ public class RestaurantProvider {
 
     public RestaurantProvider(RestaurantDao dao) {
         this.dao = dao;
+    }
+
+    public List<GetRestaurantRes> getRestaurant(Double latitude, Double longitude, String foodCategories) throws BaseException{
+        try{
+            if (latitude==null){ System.out.println("this is a Null Data2");}
+            List<GetRestaurantRes> getRestaurantRes = dao.getRestaurant(latitude, longitude, foodCategories);
+            return getRestaurantRes;
+        } catch (Exception exception) {
+            throw new BaseException(DATABASE_ERROR);
+        }
     }
 }
